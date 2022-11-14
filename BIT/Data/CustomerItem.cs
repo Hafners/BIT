6ffa81be714f505dce5c9536c4c3b2ff7303e3dd
Stefan Hafner <Stefan.Hafner@DynaDevelopment.onmicrosoft.com>
2022-11-14 18:07:03 +0100

    Add project files.

---
+++ BIT/Data/CustomerItem.cs
@@ -0,0 +1,17 @@
+﻿namespace BIT.Data
+{
+    public class CustomerItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int CustomerNumber { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public string Zipcode { get; set; }
+    }
+}
---
+++ BIT/Services/CustomerService.cs
@@ -0,0 +1,44 @@
+﻿using BIT.Data;
+using System.Collections.ObjectModel;
+
+namespace BIT.Services
+{
+    public class CustomerService
+    {
+        public CustomerService()
+        {
+            if (Customers == null)
+            {
+                Customers = new ObservableCollection<CustomerItem>()
+                {
+                    new CustomerItem { Id = 1, Name = "InfraServ GmbH", City = "Burgkirchen", Street = "Teststraße 1", Zipcode = "85632", CustomerNumber = 1},
+                    new CustomerItem { Id = 2, Name = "Otto Chemie GmbH", City = "Fridolfing", Street = "ABC Str. 23", Zipcode = "83531", CustomerNumber = 2},
+                    new CustomerItem { Id = 3, Name = "Siloking GmbH", City = "Tittmoning", Street = "Baumstraße 31", Zipcode = "84529", CustomerNumber = 3},
+                };
+            }
+        }
+
+        public ObservableCollection<CustomerItem> Customers { get; set; }
+
+        public IEnumerable<CustomerItem> ListCustomers()
+        {
+            return Customers;
+        }
+
+        public void AddCustomer(CustomerItem item)
+        {
+            Customers.Add(item);
+        }
+
+        public void UpdateCustomer(CustomerItem item)
+        {
+            var customer = Customers.Single(x => x.Id == item.Id);
+            customer = item;
+        }
+
+        public CustomerItem GetCustomer(int id)
+        {
+            return Customers.Single(x => x.Id == id);
+        }
+    }
+}
