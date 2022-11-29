aeb7a8c7d2d1c8080eb655ba34e298b1ca255358
NR-BIT <nickr474@gmail.com>
2022-11-29 09:45:53 +0100

    LieferantenItem

--- BIT/Data/LieferantItem.cs
+++ BIT/Data/LieferantItem.cs
@@ -2,6 +2,20 @@
 {
     public class LieferantItem
     {
-        public int id { get; set; }
+        public int Id { get; set; }
+
+        public string Name { get; set; } 
+
+        public string Standort { get; set; }
+
+        public string Lieferzeit { get; set; }
+
+        public int LieferantenId { get; set; }
+
+        public double Lieferkosten { get; set; }
+
+        public bool Versand { get; set; } = false;
+        
+        public bool Verfügbarkeit { get; set; } = true; 
     }
 }
