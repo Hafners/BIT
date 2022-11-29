99aa31bc93a3e4c0fd53836b7ff0dfd556bd17b3
NR-BIT <nickr474@gmail.com>
2022-11-29 09:14:48 +0100

    Lieferranten Ordner erstellt

---
+++ BIT/Data/ContactPersonItem.cs
@@ -0,0 +1,19 @@
+﻿namespace BIT.Data
+{
+    public class ContactPersonItem
+    {
+        public int Id { get; set; } 
+
+        public string FirstName { get; set; } 
+
+        public string LastName { get; set; }   
+
+        public string City { get; set; }    
+
+        public string Street { get; set; }  
+
+        public string Zipcode { get; set; } 
+
+        public string Country { get; set; } 
+    }
+}
